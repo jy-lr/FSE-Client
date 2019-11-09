7bd1b6b6edb26c51b89008d52f97c43bedbc760c
Lance Russell <lancebryanrussell@gmail.com>
2019-11-09 14:09:58 -0600

    userservice

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './Login.css';
 
-function Login(){
-    return (
-        <div className="login">
-            <div className="image-placeholder"></div>
-            <hr className="grey-bar"/>
-            <form>
-                <label htmlFor="username">Username</label>
-                <input id="username"/>
-                <label htmlFor="password">Password</label>
-                <input id="password"/>
-                <button>Login</button>
-                <button>Register</button>
-                <button>Demo</button>
-            </form>
-        </div>
-    );
+class Login extends React.Component{
+    render(){
+        return (
+            <div className="login">
+                <div className="image-placeholder"></div>
+                <hr className="grey-bar"/>
+                <form>
+                    <label htmlFor="username">Username</label>
+                    <input id="username"/>
+                    <label htmlFor="password">Password</label>
+                    <input id="password"/>
+                    <button>Login</button>
+                    <button>Register</button>
+                    <button>Demo</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Login;
