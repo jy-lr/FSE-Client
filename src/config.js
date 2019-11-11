42f71ec24bb9e82bf404602b703345e03aadd6af
Lance Russell <lancebryanrussell@gmail.com>
2019-11-11 11:55:07 -0600

    reinstated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
     API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: 'fse-auth-token',
+    TOKEN_KEY: 'auth-token',
+    DEMO_TOKEN_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE1NzMzMzgzNDUsInN1YiI6ImEifQ.qEauCcPSLIY-vWhTvHNn4ElUWg3ir15TTo94m2SvkyA'
 }
