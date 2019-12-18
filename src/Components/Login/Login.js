3a984c0892599690275419572bb3e0d7ca6f466b
Jun <jyin25@gmail.com>
2019-12-18 00:34:23 -0800

    updated login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -49,7 +49,7 @@ class Login extends React.Component{
 
   handleDemo = () => {
     userService.postLogin({
-      user_name: "SueBae",
+      user_name: "demo",
       password: "Password1!",
     })
     .then(res => {
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
     API_ENDPOINT: 'https://stark-falls-29621.herokuapp.com',
     TOKEN_KEY: 'fse-auth-token',
-    STOCK_TOKEN: 'Tpk_8d02cb5986fb405bad198d090b3ac15a',
-    DEMO_TOKEN_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1MSwidXNlcl9uYW1lIjoiZGVtbyIsImlhdCI6MTU3NjE4MzE2NCwic3ViIjoiZGVtbyJ9.hE2UPBIYP6oXi9EacL2dXOTDPVoRxmnC7upcZdMt5qk'
+    STOCK_TOKEN: 'pk_7d6d5bffd45e4914948df752e422c343',
+    DEMO_TOKEN_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VyX25hbWUiOiJ0ZXN0MSIsImlhdCI6MTU3NjE4NTA3Miwic3ViIjoidGVzdDEifQ.MABZz04TFQAjHRjScYBne4xlv3cl9mHBS-swNnIK_Tg',
+    STOCK_URL: 'https://cloud.iexapis.com/'
 }
